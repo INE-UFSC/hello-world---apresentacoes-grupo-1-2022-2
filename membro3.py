c7e55debb8fc81d2a84577d730f5f16a8c5954ea
darosabrumenzo@gmail.com <darosabrumenzo@gmail.com>
2022-08-26 11:44:00 -0300

    editei o texto

--- membro3.py
+++ membro3.py
@@ -1,4 +1,4 @@
 def hello():
 	return '''Olá, eu sou o Enzo, tenho 18 anos e meus são hobbies, principalmente, 
-	ler e jogar jogos online. Minha experiência em programação antes do curso é inexistente
+	ler a interminável Roda do Tempo e jogar jogos online. Minha experiência em programação antes do curso é inexistente
 	e eu espero aprender muitas coisas nessa disciplina'''
