7b38e47cd7916f98a5e03a948d78f5c54c9215b5
William Kraus <krwilliam03@gmail.com>
2022-08-29 20:40:54 -0300

    adiciona member5

---
+++ membro5.py
@@ -0,0 +1,14 @@
+def hello():
+	return (
+		"Olá! Meu nome é William Kraus.\n" +
+
+		"   - Tenho 19 anos e sou de Santo Amaro da Imperatriz.\n" +
+
+		"   - Eu gosto de assistir lives e vídeos (que vão desde conteúdos " +
+		"interessantes da área da computação até as maiores futilidades), ouvir música e programar!\n" +
+		
+		"   - Comecei a me interessar por programação com o início da pandemia enquanto procrastinava " +
+		"minhas tarefas do EAD. Aprendi Python principalmente, porém também já tentei desenvolvimento web.\n" +
+		
+		"   - Espero construir uma base teórica mais sólida com esta disciplina e com a graduação em geral."
+	)
