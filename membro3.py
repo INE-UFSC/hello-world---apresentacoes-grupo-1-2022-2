8b7d12b54a3a381f234cb312c26d494c376a29b0
enzoBrum <darosabrumenzo@gmail.com>
2022-08-29 20:58:52 -0300

    arrumei a formatação

--- membro3.py
+++ membro3.py
@@ -1,4 +1,7 @@
 def hello():
-	return '''Olá, eu sou o Enzo, tenho 18 anos e meus são hobbies, principalmente, 
-	ler a interminável Roda do Tempo e jogar jogos online. Minha experiência em programação antes do curso é inexistente
-	e eu espero aprender muitas coisas nessa disciplina'''
+	return '''Olá, eu sou o Enzo
+	- tenho 18 anos e sou de Laguna
+ 	- meus são hobbies, principalmente, ler a interminável Roda do Tempo e jogar jogos online. 
+  	- Minha experiência em programação antes do curso é inexistente
+	- espero melhorar os conhecimentos que já tenho e me tornar capaz de construir programas cada vez mais interessantes\n'''
+ 
