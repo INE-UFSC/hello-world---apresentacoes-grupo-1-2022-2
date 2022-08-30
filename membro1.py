349083179e5a54fcf8b73b9d57dcf0488155cf29
joao-poc <joaopedrocarvalho1973@gmail.com>
2022-08-29 21:08:02 -0300

    Membro 1 adicionado

--- membro1.py
+++ membro1.py
@@ -1,2 +1,6 @@
 def hello():
-	return "Olá, eu sou o membro1" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return """Olá, (também) me chamo João Pedro. Sou nascido no Maranhão,
+	porém criado no Pará. Minha experiência com programação é nula, mas
+	tenho bastante interesse na área, principalmente em criptografia, teoria
+	dos autômatos e semântica formal. Espero aprender o máximo que puder e 
+	desenvolver boas relações com os colegas e professores."""
